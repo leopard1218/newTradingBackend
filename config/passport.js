bbe69f922e5930292b0e5749bb9cbd8ed3620c91
leopard1218 <snowleopards1218@gmail.com>
2021-12-07 23:47:40 +0900

    second commit

--- config/passport.js
+++ config/passport.js
@@ -1,53 +1,26 @@
-// Importing Passport, strategies, and config
-const passport = require('passport'),
-  User = require('../models/user'),
-  config = require('./main'),
-  JwtStrategy = require('passport-jwt').Strategy,
-  ExtractJwt = require('passport-jwt').ExtractJwt,
-  LocalStrategy = require('passport-local');
+const JwtStrategy = require('passport-jwt').Strategy,
+  ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
 
-// Setting username field to email rather than username
-const localOptions = {
-  usernameField: 'email'
+const User = require('../models/User');
+const { jwtSecret } = require('./index');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = jwtSecret;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
 };
 
-// Setting up local login strategy
-const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
-  User.findOne({ email }, (err, user) => {
-    if (err) { return done(err); }
-    if (!user) { return done(null, false, { error: 'Your login details could not be verified. Please try again.' }); }
-
-    user.comparePassword(password, (err, isMatch) => {
-      if (err) { return done(err); }
-      if (!isMatch) { return done(null, false, { error: 'Your login details could not be verified. Please try again.' }); }
-
-      return done(null, user);
-    });
-  });
-});
-
-// Setting JWT strategy options
-const jwtOptions = {
-  // Telling Passport to check authorization headers for JWT
-  jwtFromRequest: ExtractJwt.fromAuthHeader(),
-  // Telling Passport where to find the secret
-  secretOrKey: config.secret
-
-  // TO-DO: Add issuer and audience checks
-};
-
-// Setting up JWT login strategy
-const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
-  User.findById(payload._id, (err, user) => {
-    if (err) { return done(err, false); }
-
-    if (user) {
-      done(null, user);
-    } else {
-      done(null, false);
-    }
-  });
-});
-
-passport.use(jwtLogin);
-passport.use(localLogin);
--- models/user.js
+++ models/user.js
@@ -1,79 +1,32 @@
-// Importing Node packages required for schema
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
-const ROLE_MEMBER = require('../constants').ROLE_MEMBER;
-const ROLE_CLIENT = require('../constants').ROLE_CLIENT;
-const ROLE_OWNER = require('../constants').ROLE_OWNER;
-const ROLE_ADMIN = require('../constants').ROLE_ADMIN;
+//import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
-const Schema = mongoose.Schema;
-
-//= ===============================
-// User Schema
-//= ===============================
-const UserSchema = new Schema({
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 30
+  },
   email: {
     type: String,
-    lowercase: true,
+    required: true,
     unique: true,
-    required: true
+    trim: true,
+    minlength: 5,
+    maxlength: 100
+  },
+  avatar: {
+    type: String
   },
   password: {
     type: String,
-    required: true
-  },
-  profile: {
-    firstName: { type: String },
-    lastName: { type: String }
-  },
-  role: {
-    type: String,
-    enum: [ROLE_MEMBER, ROLE_CLIENT, ROLE_OWNER, ROLE_ADMIN],
-    default: ROLE_MEMBER
-  },
-  stripe: {
-    customerId: { type: String },
-    subscriptionId: { type: String },
-    lastFour: { type: String },
-    plan: { type: String },
-    activeUntil: { type: Date }
-  },
-  resetPasswordToken: { type: String },
-  resetPasswordExpires: { type: Date }
-},
-  {
-    timestamps: true
-  });
-
-//= ===============================
-// User ORM Methods
-//= ===============================
-
-// Pre-save of user to database, hash password if password is modified or new
-UserSchema.pre('save', function (next) {
-  const user = this,
-    SALT_FACTOR = 5;
-
-  if (!user.isModified('password')) return next();
-
-  bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
-    if (err) return next(err);
-
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
-      if (err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  }
 });
 
-// Method to compare password for login
-UserSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) { return cb(err); }
-
-    cb(null, isMatch);
-  });
-};
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
